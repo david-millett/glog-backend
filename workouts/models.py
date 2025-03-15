1bdc401043e81511bd1579cf51c62a419c771c8f
David <davidmillett@protonmail.com>
2025-03-15 19:45:42 +0000

    created workout app

---
+++ workouts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
